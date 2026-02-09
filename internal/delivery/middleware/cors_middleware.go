11af2fefc65b4d47285cc2a8b9c98eddefe942d3
Donatas <ktp211zdv@yandex.ru>
2026-02-09 16:28:37 +0300

    change cors

--- internal/delivery/middleware/cors_middleware.go
+++ internal/delivery/middleware/cors_middleware.go
@@ -2,13 +2,24 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 )
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		corsOrigin := os.Getenv("CORS_ORIGIN")
+		if corsOrigin == "" {
+			corsOrigin = "*"
+		}
+		w.Header().Set("Access-Control-Allow-Origin", corsOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
